fd408c6b4dac59271bad02e9c562a823c5b4d29d
Sungsoo <onair0817@gmail.com>
2023-01-09 20:24:56 +0900

    feat: update simple node.js app

--- nodejs-docker-app/server.js
+++ nodejs-docker-app/server.js
@@ -1,12 +1,16 @@
+// express 모듈 불러오기
 const express = require('express');
 
+// express 포트 지정
 const PORT = 8080;
+const HOST = '0.0.0.0';
 
-//APP 
+//
 const app = express();
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("반가워요!!!!!!!")
 });
 
-app.listen(PORT);
+// app 서버 구동
+app.listen(PORT, HOST);
 console.log("Server is running")
